1f37ce3c3a433174bc24c8bc7d23b03c1110641d
javadkarbasian <javad.karbasian@gmail.com>
2016-03-07 05:53:23 +0330

    DetailActivity and Intent Added

--- app/src/main/java/mjkarbasianapp/com/popular_movies_version_1/ImageAdapter.java
+++ app/src/main/java/mjkarbasianapp/com/popular_movies_version_1/ImageAdapter.java
@@ -25,7 +25,7 @@ public class ImageAdapter extends BaseAdapter {
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return mThumbIds[position];
     }
 
     @Override
--- app/src/main/java/mjkarbasianapp/com/popular_movies_version_1/MovieFragment.java
+++ app/src/main/java/mjkarbasianapp/com/popular_movies_version_1/MovieFragment.java
@@ -1,10 +1,13 @@
 package mjkarbasianapp.com.popular_movies_version_1;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
 /**
@@ -18,10 +21,19 @@ public  class MovieFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        ImageAdapter imageAdapter = new ImageAdapter(getActivity());
+        final ImageAdapter imageAdapter = new ImageAdapter(getActivity());
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-        GridView gridview = (GridView) rootView.findViewById(R.id.gridview_main);
-        gridview.setAdapter(imageAdapter);
+        final GridView gridView = (GridView) rootView.findViewById(R.id.gridview_main);
+        gridView.setAdapter(imageAdapter);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Intent intent = new Intent(getActivity(),DetailActivity.class);
+                intent.putExtra("id",position);
+                startActivity(intent);
+            }
+        });
         return rootView;
     }
 }
