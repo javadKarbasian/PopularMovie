224a301c70e011a86c4d994eb52fc247eefe9598
javadkarbasian <javad.karbasian@gmail.com>
2016-04-10 05:25:06 +0430

    add listpref for sort order

--- app/src/main/java/mjkarbasianapp/com/popular_movies_version_1/MainActivity.java
+++ app/src/main/java/mjkarbasianapp/com/popular_movies_version_1/MainActivity.java
@@ -1,5 +1,6 @@
 package mjkarbasianapp.com.popular_movies_version_1;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -44,6 +45,7 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this,SettingsActivity.class));
             return true;
         }
 
